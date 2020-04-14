8b17bb9d020998afd7e0f4eff93c27a50ea61209
Christopher Lewis <cjoellewis@yahoo.com>
2020-04-14 01:17:33 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,5 @@
 # Use this space to code along with the readme
-
+def say_hello_world_ten_times
 phrase = "Hello World!"
 puts phrase
 puts phrase
@@ -11,22 +11,8 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
-
-def 
-say_hello_world_ten_times
-  phrase = "Hello World"
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
 end
-
 say_hello_world_ten_times
+
 
 
